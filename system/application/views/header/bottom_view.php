280e763af1d10631c100bdba84fc2d84171580ed
James Ming <jamesming@gmail.com>
2011-02-20 16:53:05 -0800

    fixed

--- system/application/views/header/bottom_view.php
+++ system/application/views/header/bottom_view.php
@@ -193,7 +193,7 @@
 	  >
 	<div  class=' container'   style='text-align:center;padding-top:30px;color:white;'  >
 		
-	<a href="#iframe_to_edit_div"  class="iframe_to_edit_div_link"  style='color:white'  href='<?php echo base_url();    ?>index.php?/home/about'>About</a> &nbsp;&nbsp;<a  style='color:white'  href='mail:corkhub@gmail.com'>Contact Us</a>&nbsp;&nbsp;&copy;&nbsp;Copyright 2011<br>
+	<a href="#iframe_to_edit_div"  class="iframe_to_edit_div_link"  style='color:white'  href='<?php echo base_url();    ?>index.php?/home/about'>About</a> &nbsp;&nbsp;<a  style='color:white'  href='mailto:corkhub@gmail.com'>Contact Us</a>&nbsp;&nbsp;&copy;&nbsp;Copyright 2011<br>
 		
 	</div>
 
